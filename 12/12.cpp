f0d48d0c4dc65627e847e1813695a90a4f37d06d
Benas Budrys <29711974+BenasB@users.noreply.github.com>
2019-01-10 23:25:15 +0200

    Problem 12 added

--- 12/12.cpp
+++ 12/12.cpp
@@ -2,7 +2,32 @@
 
 using namespace std;
 
+int factors(long long a)
+{
+    int n = 0;
+    for (long long i = 1; i*i <= a; i++)
+    {
+        if (a % i == 0)
+        {
+            if (i*i < a)
+                n += 2;
+            else
+                n++;
+        }
+    }
+
+    return n;
+}
+
 int main()
 {
+    long long a = 1;
+    long long b = 2;
+    while(factors(a) <= 500)
+    {
+        a += b;
+        b++;
+    }
 
+    cout << a << endl;
 }
